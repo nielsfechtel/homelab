06a482ca4158ebeed6b0f04222d1bd3a0416438c
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-05-04 22:42:10 +0000

    Adjust config-patch to include the settings required by Longhorn

--- scripts/create-talos-configs.sh
+++ scripts/create-talos-configs.sh
@@ -15,9 +15,13 @@ echo "API endpoint: https://${ENDPOINT}:6443"
 # Generate the Talos config files for control plane and worker nodes
 # Patch to use /dev/vda instead of default /dev/sda, see
 # https://www.talos.dev/v1.9/talos-guides/configuration/patching/#configuration-patching-with-talosctl-cli
+patch_disk='{"op": "replace", "path": "/machine/install/disk", "value": "/dev/vda"}'
+# As specified in https://longhorn.io/docs/1.8.1/advanced-resources/os-distro-specific/talos-linux-support/#data-path-mounts
+patch_longhorn='{"op": "replace", "path": "/machine/kubelet/extraMounts", "value": [ { "destination": "/var/lib/longhorn", "type": "bind", "source": "/var/lib/longhorn", "options": [ "bind", "rshared", "rw" ] } ] }'
+
 talosctl gen config "$CLUSTER_NAME" "https://${ENDPOINT}:6443" \
   --output-dir "${REPO_ROOT}/talos/configs" \
-  --config-patch '[{"op": "replace", "path": "/machine/install/disk", "value": "/dev/vda"}]' 
+  --config-patch "[${patch_disk}, ${patch_longhorn}]"
 
 # Adjust local talosconfig to include endpoint- and node-IPs (array to whitespace-separated list)
 talosctl config endpoints $(IFS=" "; echo "${CONTROLPLANE_IPS[*]}")
