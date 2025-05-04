88450575e1098c5e3f892384c1f0da9b8ddfd0e6
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-05-04 23:00:21 +0000

    Do renovate-testing another time/branch

--- terraform/proxmox/versions.tf
+++ terraform/proxmox/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
-      version = "2.9.12"
+      version = "3.0.1-rc8"
     }
     sops = {
       source = "carlpett/sops"
